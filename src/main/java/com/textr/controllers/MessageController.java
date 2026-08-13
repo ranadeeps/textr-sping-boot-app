de4445ab8cd2f5b78b93b2ba879c34a991edcb7c
ranadeeps <ranadeep2104@gmail.com>
2026-08-13 13:08:21 +0530

    message handling

--- src/main/java/com/textr/controllers/MessageController.java
+++ src/main/java/com/textr/controllers/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,21 +18,24 @@ public class MessageController {
     @Autowired
     private ChatRoomService chatRoomService;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @PostMapping("/save-message")
     public ResponseEntity<Message> saveMessage(@RequestBody MessageDTO request) {
         return new ResponseEntity<>(chatRoomService.saveMessage(request.getRoomId(), request.getContent()), HttpStatus.CREATED);
     }
 
     @MessageMapping("/{roomId}")
-    @SendTo("/room/{roomId}")
-    public MessageDTO sendMessage(@DestinationVariable String roomId, @Payload MessageDTO message) {
+    public void sendMessage(@DestinationVariable String roomId, @Payload MessageDTO message) {
         chatRoomService.saveMessage(roomId, message.getContent());
-        return message;
+        String destinationTopic = "/room/" + roomId;
+        messagingTemplate.convertAndSend(destinationTopic, message);
     }
 
     @MessageExceptionHandler
     public void handleException(Exception exception) {
-        System.out.println(exception.getMessage());
+        System.err.println("=== WEBSOCKET CONTROLLER ERROR ===");
+        exception.printStackTrace();
     }
-
 }
